15f02e73870a3c55a2e2415667abe03eec56f624
ilya <yuga.ferg@gmail.com>
2023-10-03 14:25:53 +0300

    footer

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -1,20 +1,25 @@
 <footer class="footer">
-    <a href="" class="d-flex justify-content-center mb-px-20">
-        <img src="{{asset('img/logo.png')}}" width="50" alt="">
-    </a>
-    <div class="d-flex align-items-center flex-column flex-md-row text-center justify-content-center mb-px-20" style="gap: 20px">
-        <a href="" class="footer-link">Вакуум</a>
-        <a href="" class="footer-link">Литьё</a>
-        <a href="" class="footer-link">Силикон</a>
-        <a href="" class="footer-link">Матрицы</a>
-        <a href="" class="footer-link">Прототипы</a>
-{{--        <a href="{{route('service.contacts')}}" class="footer-link">Контакты</a>--}}
-    </div>
-    <div class="col-md-5">
-        <div>qwd</div>
-        <div>qwd</div>
-        <div>qwd</div>
-        <div>qwd</div>
+{{--    <a href="" class="d-flex justify-content-center mb-px-20">--}}
+{{--        <img src="{{asset('img/logo.png')}}" width="50" alt="">--}}
+{{--    </a>--}}
+    <div class="container-xl">
+        <div class="row d-flex">
+            <div class="col-2">
+                <img src="{{asset('img/logo.webp')}}" class="mb-3">
+                <div class="foot-txt pl-px-10">
+                    Компания <b>Стамик</b> - это разработка и производство пресс-форм,
+                    прототипов, мастер моделей. Для литья и вакуумной формовки.
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="foot-title pl-px-10">
+                    ФОРМЫ
+                    <div class="foot-hr-line"></div>
+                </div>
+            </div>
+
+        </div>
+
     </div>
     <div class="text-center text-muted px-3">
         Лучшая реклама любой работы - её качество.
