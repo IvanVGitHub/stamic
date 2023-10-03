3c69685c983d96804117da61710b1886f335ddea
ilya <yuga.ferg@gmail.com>
2023-10-03 17:51:32 +0300

    footer

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -3,25 +3,77 @@
 {{--        <img src="{{asset('img/logo.png')}}" width="50" alt="">--}}
 {{--    </a>--}}
     <div class="container-xl">
-        <div class="row d-flex">
-            <div class="col-2">
-                <img src="{{asset('img/logo.webp')}}" class="mb-3">
-                <div class="foot-txt pl-px-10">
+        <div class="row g-4 d-flex">
+            <div class="col-md-6 col-lg-4 col-xl-2">
+                <img src="{{asset('img/logo.webp')}}" class="d-flex footer-logo mb-3 justify-content-center">
+                <div class="foot-txt pl-px-10 mb-2">
                     Компания <b>Стамик</b> - это разработка и производство пресс-форм,
                     прототипов, мастер моделей. Для литья и вакуумной формовки.
                 </div>
             </div>
-            <div class="col-2">
-                <div class="foot-title pl-px-10">
-                    ФОРМЫ
-                    <div class="foot-hr-line"></div>
+            <div class="col-md-6 col-lg-4 col-xl-2">
+                <div class="footer-block">
+                    <div class="foot-title">
+                        ФОРМЫ
+                        <div class="foot-hr-line"></div>
+                    </div>
+                    <div class="">
+                        <a href="" class="footer-linko ">Вакуум</a>
+                        <a href="" class="footer-linko">Литье</a>
+                        <a href="" class="footer-linko ">Силикон</a>
+                        <a href="" class="footer-linko ">Матрицы</a>
+                        <a href="" class="footer-linko ">Прототипы</a>
+                    </div>
                 </div>
             </div>
+            <div class=" col-md-6 col-lg-4 col-xl-3">
+                <div class="footer-block">
+                    <div class="foot-title">
+                        ТЕХНОЛОГИИ
+                    </div>
+                    <div class="foot-hr-line"></div>
+                    <div class="">
+                        <a href="" class="footer-linko d-flex">3D моделирование</a>
+                        <a href="" class="footer-linko">3D печать</a>
+                        <a href="" class="footer-linko">3D сканирование</a>
+                        <a href="" class="footer-linko">Фрезеровка</a>
+                        <a href="" class="footer-linko">Литье под давлением</a>
+                        <a href="" class="footer-linko">Вакуумная формовка</a>
+                    </div>
+                </div>
 
+            </div>
+            <div class=" col-md-6 col-lg-4 col-xl-3">
+                <div class="footer-block with-line">
+                    <div class="foot-title">
+                        СВЯЖИТЕСЬ С НАМИ
+                    </div>
+                    <div class="foot-hr-line"></div>
+                    <div class="">
+                        <a href="" class="footer-linko d-flex"><span class="text-small">Тульская обл. г.
+                                Кимовск ул. Октябрьская, 19</span></a>
+                        <a href="" class="footer-linko"><span class="text-orange">+7 950 912 05 91</span></a>
+                        <a href="" class="footer-linko"><span class="text-orange">+7 952 018 33 46</span></a>
+                        <div href="" class="footer-linko"><span class="text-small">с 9.00-18.00 Пн-Пт</span></div>
+                        <a href="" class="footer-linko"><span class="text-orange">info@stamic.ru</span></a>
+                    </div>
+                </div>
+            </div>
+            <div class=" col-md-6 col-lg-4 col-xl-3">
+                <div class="footer-block">
+                    <div class="foot-title">
+                        ИНФОРМАЦИЯ
+                    </div>
+                    <div class="foot-hr-line"></div>
+                    <div class="">
+                        <a href="" class="footer-linko d-flex">О нас</a>
+                        <a href="" class="footer-linko">Проекты</a>
+                        <a href="" class="footer-linko">Контакты</a>
+                    </div>
+                </div>
+
+            </div>
         </div>
+    </div>
 
-    </div>
-    <div class="text-center text-muted px-3">
-        Лучшая реклама любой работы - её качество.
-    </div>
 </footer>
