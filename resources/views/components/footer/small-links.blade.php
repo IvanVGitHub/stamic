282ec574843b7b7a84bb24b9112552f77c4edb8a
ilya <yuga.ferg@gmail.com>
2023-10-05 15:56:07 +0300

    footer finish

--- resources/views/components/footer/small-links.blade.php
+++ resources/views/components/footer/small-links.blade.php
@@ -1,4 +1,4 @@
-<div class="mt-md-5 mt-2 align-items-center d-flex" >
+<div class="align-items-center d-flex" >
     <a href="">
         <img src="{{asset('img/telegram.png')}}" class="messenger" style="margin-right: 10px">
     </a>
