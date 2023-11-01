2e52917f68672c79c4015050eb42b3e9c092be0f
Ivan <virus_911@mail.ru>
2023-11-01 18:18:57 +0300

    Мобильная версия Шапки.

--- resources/views/components/layout/header/mobile-side-menu.blade.php
+++ resources/views/components/layout/header/mobile-side-menu.blade.php
@@ -2,55 +2,39 @@
     <div class="header-mobile-content p-2" onclick="preventToggelMobileMenu(this)">
         <div class="mb-3 d-flex justify-content-between align-items-center position-absolute top-0 start-0 w-100 p-2 bg-default"
              style="z-index:300; height: 56px">
-            <a href="{{--{{route('main')}}--}}" class="header-mobile-logo">
-                <img src="{{--{{asset(config('utility.animatedLogo'))}}--}}" class="main-logo-image" alt="Autlet shin">
+            <a href="{{route('main')}}" class="header-mobile-logo">
+                <img src="{{asset('img/logo-light.webp')}}" class="main-logo-image w-100" alt="stamic">
             </a>
             <button class="header-mobile-button" onclick="toggleMobileMenu()"><i class="fa-solid fa-xmark"></i></button>
         </div>
         <div class="header-menu-main-content pt-px-50">
-            <a href="{{--{{route('main')}}--}}" class="header-link">
+            <a href="{{route('main')}}" class="header-link">
                 Главная
             </a>
-            <a href="{{--{{route('service.about')}}--}}" class="header-link">
-                О студии
+            <a href="{{route('uslugi')}}" class="header-link">
+                Формы
             </a>
-            <a href="{{--{{route('service.price')}}--}}" class="header-link">
-                Прайс-лист
+            <a href="{{route('tehnologii')}}" class="header-link">
+                Технологии
             </a>
-            @can('ap_access')
-                <a href="{{--{{route('adm.index')}}--}}" class="header-link">
-                    Адм. панель
-                </a>
-            @endcan
-            {{--            <a href="{{route('service.contacts')}}" class="header-link mb-3">--}}
-            {{--                Контакты--}}
-            {{--            </a>--}}
+            <a href="{{route('proekty')}}" class="header-link">
+                Проекты
+            </a>
+            <a href="{{route('kontakty')}}" class="header-link mb-3">
+                Контакты
+            </a>
+
             <div class="mt-4">
-                <a href="{{--{{route('landings.webdev')}}--}}"
-                   class="header-link">Разработка сайтов</a>
-                <a class="header-link ms-3" href="{{--{{route("landings.landing-page")}}--}}">Лендинг</a>
-                <a class="header-link ms-3" href="{{--{{route("landings.personal-page")}}--}}">Сайт-визитка</a>
-                <a class="header-link ms-3" href="{{--{{route("landings.shop")}}--}}">Интернет-магазин</a>
-                <a class="header-link ms-3" href="{{--{{route("landings.custom")}}--}}">Сайт под заказ</a>
-                <a class="header-link ms-3" href="{{--{{route("landings.modification")}}--}}">Переработка сайта</a>
-                <a class="header-link ms-3" href="{{--{{route("landings.design")}}--}}">Web-дизайн</a>
-            </div>
-            <div class="">
-                <a href="{{--{{route('landings.adv')}}--}}"
-                   class="header-link">Продвижение сайтов</a>
-                <a class="header-link ms-3" href="{{--{{route('landings.seo')}}--}}">SEO</a>
-                <a class="header-link ms-3" href="{{--{{route('landings.smo')}}--}}">Продвижение в соц-сетях</a>
+                <div class="header-link">Связаться с нами</div>
+                <a href="tel:+79509120591" class="header-link ms-3">+7 950 912 05 91</a>
+                <a href="tel:+79520183346" class="header-link ms-3">+7 952 018 33 46</a>
             </div>
 
-
-            <button type="button" data-bs-target="#callForm" data-bs-toggle="modal" class="header-link mt-px-20 ">
-                <i class="fa-solid fa-phone"></i>&nbsp;&nbsp;Связаться с нами
-            </button>
-            <a href="{{--{{route('service.contacts')}}--}}" class="header-link">
-                <i class="fa-solid fa-envelope"></i>&nbsp;&nbsp;Контакты
+            <a href="https://t.me/AndreMatro" class="header-link mt-px-20">
+                <i class="fa-solid fa-envelope"></i>&nbsp;&nbsp;Телеграм
             </a>
-            <a href="{{--{{route('service.credentials')}}--}}" class="header-link">
-                <i class="fa-solid fa-circle-info"></i>&nbsp;&nbsp;Реквизиты
+            <a href="https://www.instagram.com/stamic_ru/" class="header-link">
+                <i class="fa-solid fa-circle-info"></i>&nbsp;&nbsp;Фото
             </a>
         </div>
     </div>
