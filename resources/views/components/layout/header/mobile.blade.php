48e021e6675c6a70947ece4eb2285373fa2d2fde
Ivan <virus_911@mail.ru>
2023-11-10 14:45:22 +0300

    ГОТОВО. Мобильная версия Хедера.

--- resources/views/components/layout/header/mobile.blade.php
+++ resources/views/components/layout/header/mobile.blade.php
@@ -1,8 +1,14 @@
 <div class="header-mobile">
     <div class="d-flex justify-content-between align-items-center p-2">
-        <a href="{{route('main')}}" class="header-mobile-logo">
-            <img src="{{asset('img/favicon.png')}}" alt="Stamic">
-        </a>
+        <div class="d-flex gap-2 align-items-center">
+            <a href="{{route('main')}}" class="header-mobile-logo">
+                <img src="{{asset('img/favicon.png')}}" alt="Stamic">
+            </a>
+            <div class="text-size-18">
+                @yield('header-title')
+            </div>
+        </div>
+
         <div class="d-flex gap-2">
             <a href="tel:+79509120591" class="header-mobile-button">
                 <i class="fa-solid fa-phone fa-lg"></i>
