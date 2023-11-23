f34001b3fb6e6b1dabee36d7e914f45b2d49e1cc
Ivan <virus_911@mail.ru>
2023-11-23 13:00:08 +0300

    *Установил имя почты владельца сайта.

--- config/utility.php
+++ config/utility.php
@@ -5,5 +5,5 @@ return [
     'phone-show'=>'+7 950 912 05 91',
     'phone2'=>'+79520183346',
     'phone2-show'=>'+7 952 018 33 46',
-    'emailOperator'=>'virus_911@mail.ru',
+    'emailOperator'=>'8402392@mail.ru',
 ];
